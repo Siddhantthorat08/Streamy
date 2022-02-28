a3d286a0c8e286d49427d510caf1686ac505d46f
Siddhantthorat08 <siddhant08thorat10@gmail.com>
2022-02-28 15:02:13 +0530

    Initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,55 @@
+import formValues from 'redux-form/lib/formValues';
+import streams from '../apis/streams';
+import history from '../history';
+
+export const signIn = (userId) => {
+    return {
+        type: "SIGN_IN",
+        payload: userId
+    }   
+}
+
+export const signOut = () => {
+    return {
+        type: "SIGN_OUT"
+    }   
+}
+
+export const createStream = (formValues) => {
+    return async (dispatch, getState) => {
+        const { userId } = getState().auth;
+        const response = await streams.post('/streams',{...formValues, userId});
+        dispatch({type: "CREATE_STREAM", payload: response.data})
+        history.push('/')
+    }
+}
+
+export const fetchStreams = () => { 
+    return async (dispatch) => {
+        const response = await streams.get('/streams');
+        dispatch({ type: "FETCH_STREAMS", payload: response.data })
+    }
+}
+
+export const fetchStream = (id) => {
+    return async (dispatch) => {
+        const response = await streams.get(`/streams/${id}`);
+        dispatch({ type: "FETCH_STREAM", payload: response.data })
+    }
+}
+
+export const editStream = (id,formValues) => {
+    return async (dispatch) => {
+        const response = await streams.patch(`/streams/${id}`, formValues);
+        dispatch({ type: "EDIT_STREAM", payload: response.data });
+        history.push('/')
+    }
+}
+
+export const deleteStreams = (id) => {
+    return async (dispatch) => {
+        await streams.delete(`/streams/${id}`);
+        dispatch({ type: "DELETE_STREAMS", payload: id });
+    }
+}
+
---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+// Client ID
+// 450981544729-21p74q1d1rhfmicv8tnb80d6vgal82a6.apps.googleusercontent.com
+
+import React from "react";
+import { Router, Route, Link } from "react-router-dom";
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from "./Header";
+import history from "../history";
+
+const App = () => {
+    return(
+        <div className = "ui container">
+            <Router history = {history}>
+                <div>
+                    <Header />
+                    <Route path = "/" exact component = {StreamList} />
+                    <Route path = "/stream/new" exact component = {StreamCreate} />
+                    <Route path = "/stream/edit/:id" exact component = {StreamEdit} />
+                    <Route path = "/stream/delete/:id" exact component = {StreamDelete} />
+                    <Route path = "/stream/show" exact component = {StreamShow} />
+                </div>
+            </Router>
+        </div>
+    )
+}
+
+export default App
